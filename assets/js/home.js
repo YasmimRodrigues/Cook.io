86a232f0121f0f9ab139299768f844a58b849a27
YasmimRodrigues <yasmimrbm25@gmail.com>
2025-11-10 11:05:18 -0300

    feat: implement search button logic to redirect to recipes page with query

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,3 +5,14 @@
  */
 
 "use strict";
+
+/** 
+ * Home page Search
+ */
+
+const /** {NodeElement} */ $searchField = document.querySelector("[data-search-field]");
+const /** {NodeElement} */ $searchBtn = document.querySelector("[data-search-btn");
+
+$searchBtn.addEventListener("click", function () {
+    if($searchField.value) window.location = `/recipes.html?q=${$searchField.value}`;
+});
