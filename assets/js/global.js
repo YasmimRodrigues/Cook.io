1d63b51ab257cb8774b9d8dfca42ac7cb522a341
YasmimRodrigues <yasmimrbm25@gmail.com>
2025-11-11 10:40:44 -0300

    refactor: create reusable addEventOnElements utility function

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,3 +5,16 @@
  */
 
 "use strict";
+
+/**
+ * Add event on multiple elements
+ * @param {NodeList} $elements NodeList
+ * @param {String} eventype Event type string
+ * @param {Function} callback Callback function
+ */
+
+window.addEventOnElements = ($elements, eventype, callback) => {
+    for (const $element of $elements) {
+        $element.addEventListener(eventype, callback);
+    }
+}
