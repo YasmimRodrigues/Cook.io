2b0622e6eaaa722462e85f482e6359b4bf5c62a2
YasmimRodrigues <yasmimrbm25@gmail.com>
2025-12-09 10:38:32 -0300

    feat(detail): initialize recipe detail rendering logic

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -5,3 +5,49 @@
  */
 
 "use strict";
+
+/**
+ * Import
+ */
+
+import { fetchData } from "./api.js";
+import { getTime } from "./module.js";
+
+/**
+ * Render data
+ */
+
+const /** {NodeElement} */ $detailContainer = document.querySelector("[data-detail-container]");
+
+const /** {String} */ recipeId = window.location.search.slice(window.location.search.indexOf("=") + 1);
+const /** {String} */ detailUrl = `${ACCESS_POINT}/${recipeId}`;
+
+fetchData(null, data => {
+
+    console.log(data);
+    
+    // Guard Clause
+    
+    if(!data || !data.recipe) return;
+
+    const {
+        images: { 
+            LARGE = '', 
+            REGULAR = '', 
+            SMALL = '', 
+            THUMBNAIL = ''
+        } = {},
+        label: title,
+        source: author,
+        ingredients = [],
+        totalTime: cookingTime = 0,
+        calories = 0,
+        cuisineType = [],
+        dietLabels = [],
+        dishType = [],
+        yield: servings = 0,
+        ingredientLines = [],
+        uri
+    } = data.recipe;
+
+}, detailUrl);
